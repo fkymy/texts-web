1e0d2dab4caa56bfcc177dd67cd5cb5c7e124e9a
Yuske Fukuyama <yuskefuku@gmail.com>
2020-09-12 21:35:12 +0900

    kubi itai

--- src/components/GlobalStyle.tsx
+++ src/components/GlobalStyle.tsx
@@ -10,53 +10,53 @@ import { DARK_MODE } from '../constants/mediaQuery';
 
 export default function GlobalStyle() {
   const GLOBAL_STYLE = css`
-    html {
-      background-color: ${LIGHT_BACKGROUND_COLOR};
+    // html {
+    //   background-color: ${LIGHT_BACKGROUND_COLOR};
 
-      ${DARK_MODE} {
-        background-color: ${DARK_BACKGROUND_COLOR};
-      }
-    }
+    //   ${DARK_MODE} {
+    //     background-color: ${DARK_BACKGROUND_COLOR};
+    //   }
+    // }
 
-    body {
-      margin: 0;
-      color: ${LIGHT_FOREGROUND_COLOR};
+    // body {
+    //   margin: 0;
+    //   color: ${LIGHT_FOREGROUND_COLOR};
 
-      ${DARK_MODE} {
-        color: ${DARK_FOREGROUND_COLOR};
-      }
-    }
+    //   ${DARK_MODE} {
+    //     color: ${DARK_FOREGROUND_COLOR};
+    //   }
+    // }
 
-    h1,
-    h2,
-    h3,
-    h4,
-    h5,
-    h6 {
-      font-size: inherit;
-      margin-block-start: 0;
-      margin-block-end: 0;
-      margin-inline-start: 0;
-      margin-inline-end: 0;
-      font-weight: inherit;
-    }
+    // h1,
+    // h2,
+    // h3,
+    // h4,
+    // h5,
+    // h6 {
+    //   font-size: inherit;
+    //   margin-block-start: 0;
+    //   margin-block-end: 0;
+    //   margin-inline-start: 0;
+    //   margin-inline-end: 0;
+    //   font-weight: inherit;
+    // }
 
-    ul,
-    ol {
-      display: inline;
-      margin-block-start: 0;
-      margin-block-end: 0;
-      padding-inline-start: 0;
-      padding-inline-end: 0;
-    }
+    // ul,
+    // ol {
+    //   display: inline;
+    //   margin-block-start: 0;
+    //   margin-block-end: 0;
+    //   padding-inline-start: 0;
+    //   padding-inline-end: 0;
+    // }
 
-    li {
-      display: inline;
-    }
+    // li {
+    //   display: inline;
+    // }
 
-    a {
-      text-decoration: none;
-    }
+    // a {
+    //   text-decoration: none;
+    // }
   `;
   return <Global styles={GLOBAL_STYLE} />;
 }
